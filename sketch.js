00ce08784004d5e8df3e1c0de3fc0d42db8ceef1
AsedaDeveloper <74008800+AsedaDeveloper@users.noreply.github.com>
2021-01-02 19:34:46 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const startBtn = document.createElement("button");
 startBtn.innerHTML = "Start listening";
 const result = document.createElement("div");
 const processing = document.createElement("p");
-document.write("<body><h1>My Siri</h1><p>Give it a try with 'hello', 'how are you', 'what's your name', 'what time is it', 'stop', ... </p></body>");
+document.write("<body><h1>Intruction to (My Siri) </h1><p>Give it a try with 'hello', 'how are you', 'what's your name', 'what time is it', 'what is github', 'thank you', stop' try saying hey siri and then say what time is it, ... </p></body>");
 document.body.append(startBtn);
 document.body.append(result);
 document.body.append(processing);
@@ -58,14 +58,27 @@ function process(rawText) {
 	text = text.toLowerCase();
 	let response = null;
 	switch(text) {
+        case "whatisgithub":
+			response = "GitHub, Inc. is a subsidiary of Microsoft which provides hosting for software development and version control using Git. It offers the distributed version control and source code management functionality of Git, plus its own features."; break;
+        case "whattimeisit":
+			response = new Date().toLocaleTimeString(); break;            
 		case "hello":
 			response = "hi, how are you doing?"; break;
 		case "what'syourname":
 			response = "My name's Siri.";  break;
 		case "howareyou":
 			response = "I'm good."; break;
-		case "whattimeisit":
-			response = new Date().toLocaleTimeString(); break;
+		case "heysiri":
+			response = "How can i help you."; break;  
+		case "awesome":
+			response = "Thanks."; break;              
+        case "howareyoudoing":
+			response = "Hello, i'm fine thanks."; break;              
+		case "howoldareyou":
+			response = "I am as old as the eastern wind, and a young as a newborn caterpillar."; break; 
+		case "thankyou":
+			response = "You're welcome!!";
+			toggleBtn();  
 		case "stop":
 			response = "Bye!!";
 			toggleBtn();
